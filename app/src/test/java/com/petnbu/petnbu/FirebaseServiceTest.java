1a74967b3ec70caefb05107a6d4db208b3512953
Pham Sang <phamngocquangsang@gmail.com>
2018-04-23 11:35:17 +0700

    Add test feed api create/update and paging

---
+++ app/src/test/java/com/petnbu/petnbu/FirebaseServiceTest.java
@@ -0,0 +1,11 @@
+package com.petnbu.petnbu;
+
+import com.petnbu.petnbu.model.Feed;
+
+import org.junit.Test;
+
+public class FirebaseServiceTest {
+    @Test
+    public void testCreatePost(){
+    }
+}
