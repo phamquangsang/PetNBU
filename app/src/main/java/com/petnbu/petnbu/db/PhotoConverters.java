7d4f4502c52b12fd45be21c78ab2693cddc0e1ad
Pham Sang <phamngocquangsang@gmail.com>
2018-05-24 15:38:50 +0700

    Add missing Converters

--- app/src/main/java/com/petnbu/petnbu/db/PhotoConverters.java
+++ app/src/main/java/com/petnbu/petnbu/db/PhotoConverters.java
@@ -1,4 +1,23 @@
 package com.petnbu.petnbu.db;
 
+import android.arch.persistence.room.TypeConverter;
+
+import com.google.gson.Gson;
+import com.petnbu.petnbu.PetApplication;
+import com.petnbu.petnbu.model.Photo;
+
 public class PhotoConverters {
+
+    public static Gson gson = PetApplication.getAppComponent().getGson();
+
+    @TypeConverter
+    public static String photoToJson(Photo photo){
+        Gson gson = new Gson();
+        return gson.toJson(photo);
+    }
+
+    @TypeConverter
+    public static Photo jsonToPhoto(String photoJson){
+        return gson.fromJson(photoJson, Photo.class);
+    }
 }
