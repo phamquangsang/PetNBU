fbd4e7e7e23c2719abb82452d8238231e61cdf83
Nhat Pham <evichrock@gmail.com>
2018-05-30 11:37:14 +0700

    show loading for notifications

--- app/src/main/java/com/petnbu/petnbu/notification/NotificationViewModel.java
+++ app/src/main/java/com/petnbu/petnbu/notification/NotificationViewModel.java
@@ -1,16 +1,20 @@
 package com.petnbu.petnbu.notification;
 
 import android.app.Application;
+import android.arch.core.util.Function;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Transformations;
 import android.arch.lifecycle.ViewModel;
+import android.databinding.ObservableBoolean;
 
 import com.petnbu.petnbu.PetApplication;
 import com.petnbu.petnbu.SharedPrefUtil;
 import com.petnbu.petnbu.api.WebService;
 import com.petnbu.petnbu.model.NotificationUI;
 import com.petnbu.petnbu.model.Resource;
+import com.petnbu.petnbu.model.Status;
 import com.petnbu.petnbu.repo.CommentRepository;
-import com.petnbu.petnbu.repo.FeedRepository;
 import com.petnbu.petnbu.repo.NotificationRepository;
 import com.petnbu.petnbu.repo.UserRepository;
 
@@ -36,12 +40,26 @@ public class NotificationViewModel extends ViewModel {
     @Inject
     Application mApplication;
 
+    public final ObservableBoolean showLoading = new ObservableBoolean();
+
     public NotificationViewModel() {
         PetApplication.getAppComponent().inject(this);
     }
 
-    public LiveData<Resource<List<NotificationUI>>> getNotifications(){
-        return mNotificationRepository.getUserNotifications(SharedPrefUtil.getUserId(), new Date().getTime());
+    public LiveData<List<NotificationUI>> getNotifications(){
+        return Transformations.switchMap(mNotificationRepository.getUserNotifications(SharedPrefUtil.getUserId(), new Date().getTime()), new Function<Resource<List<NotificationUI>>, LiveData<List<NotificationUI>>>() {
+            @Override
+            public LiveData<List<NotificationUI>> apply(Resource<List<NotificationUI>> notificationsResource) {
+                MutableLiveData<List<NotificationUI>> notificationsLiveData = new MutableLiveData<>();
+                if(notificationsResource != null){
+                    showLoading.set(notificationsResource.status == Status.LOADING);
+                    if(notificationsResource.data != null){
+                        notificationsLiveData.setValue(notificationsResource.data);
+                    }
+                }
+                return notificationsLiveData;
+            }
+        });
     }
 
 }
--- app/src/main/java/com/petnbu/petnbu/notification/NotificationsFragment.java
+++ app/src/main/java/com/petnbu/petnbu/notification/NotificationsFragment.java
@@ -13,17 +13,15 @@ import android.view.ViewGroup;
 
 import com.petnbu.petnbu.R;
 import com.petnbu.petnbu.databinding.FragmentNotificationsBinding;
-import com.petnbu.petnbu.model.Status;
 
 import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
 
+    private NotificationViewModel mNotificationViewModel;
+
     private FragmentNotificationsBinding mBinding;
-
     private NotificationsRecyclerViewAdapter mAdapter;
-
-    private NotificationViewModel mViewModel;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -34,69 +32,17 @@ public class NotificationsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_notifications, container, false);
-        mViewModel = ViewModelProviders.of(this).get(NotificationViewModel.class);
         initialize();
         return mBinding.getRoot();
     }
 
     private void initialize() {
-//        List<Notification> notificationList = new ArrayList<>();
-//        Notification notification = new Notification();
-//        notification.setId("1");
-//
-//        FeedUser feedUser = new FeedUser();
-//        feedUser.setUserId("1");
-//        feedUser.setName("Nhat Pham");
-//        feedUser.setAvatarUrl("https://academy-stg-assets.s3.amazonaws.com/user_50/hN5JRUzjs8nXifDEWnBc1522315546980_optimized.jpg");
-//        notification.setFromUser(feedUser);
-//        notification.setTimeCreated(Calendar.getInstance().getTime());
-//        notification.setType(Notification.TYPE_LIKE_FEED);
-//        notificationList.add(notification);
-//
-//        notification = new Notification();
-//        notification.setId("1");
-//        notification.setFromUser(feedUser);
-//        notification.setTimeCreated(Calendar.getInstance().getTime());
-//        notification.setType(Notification.TYPE_LIKE_COMMENT);
-//        notificationList.add(notification);
-//
-//
-//        notification = new Notification();
-//        notification.setId("1");
-//        notification.setFromUser(feedUser);
-//        notification.setTimeCreated(Calendar.getInstance().getTime());
-//        notification.setType(Notification.TYPE_LIKE_REPLY);
-//        notificationList.add(notification);
-//
-//        notification = new Notification();
-//        notification.setId("1");
-//        notification.setFromUser(feedUser);
-//        notification.setTimeCreated(Calendar.getInstance().getTime());
-//        notification.setType(Notification.TYPE_NEW_COMMENT);
-//        notificationList.add(notification);
-//
-//        notification = new Notification();
-//        notification.setId("1");
-//        notification.setFromUser(feedUser);
-//        notification.setTimeCreated(Calendar.getInstance().getTime());
-//        notification.setType(Notification.TYPE_NEW_REPLY);
-//        notificationList.add(notification);
+        mNotificationViewModel = ViewModelProviders.of(this).get(NotificationViewModel.class);
+        mBinding.setViewModel(mNotificationViewModel);
+        mNotificationViewModel.getNotifications().observe(this, notifications -> mAdapter.setData(notifications));
 
         mBinding.rvNotifications.setLayoutManager(new LinearLayoutManager(getActivity()));
         mAdapter = new NotificationsRecyclerViewAdapter(getActivity(), new ArrayList<>());
         mBinding.rvNotifications.setAdapter(mAdapter);
-
-        mViewModel.getNotifications().observe(this, listResource -> {
-            if(listResource != null){
-                if(listResource.data != null){
-                    mAdapter.setData(listResource.data);
-                }
-                if(listResource.status == Status.LOADING){
-                    //todo show loading
-                }else{
-                    //todo hide loading
-                }
-            }
-        });
     }
 }
