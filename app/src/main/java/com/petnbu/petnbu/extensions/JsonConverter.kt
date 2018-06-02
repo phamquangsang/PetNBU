94bcc973c28e49fdaa2eb8a9fa605334251a807e
Nhat Pham <evichrock@gmail.com>
2018-06-02 14:02:38 +0700

    fix bug cannot create feed

--- app/src/main/java/com/petnbu/petnbu/extensions/JsonConverter.kt
+++
@@ -1,8 +0,0 @@
-package com.petnbu.petnbu.extensions
-
-import com.google.gson.Gson
-import com.petnbu.petnbu.model.Photo
-
-inline fun Photo.toJson(): String = Gson().toJson(this)
-
-inline fun List<Photo>.toJson(): String = Gson().toJson(this)
